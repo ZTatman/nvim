1fabd91df85285f544bde8075eb5f019227f792d
ZTatman <tatmanzachariahit@gmail.com>
2025-11-17 15:38:30 -0500

    feat: update config files and add which-key configuration
    - Add which-key configuration for better keymap organization
    - Update init.lua, autocmds, conform, lspconfig
    - Update functions, mappings, and plugins
    - Add PR documentation files

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,13 +1,13 @@
 local autocmd = vim.api.nvim_create_autocmd
 
--- Format on save
-autocmd("BufWritePre", {
-  pattern = "*.lua",
-  callback = function()
-    -- vim.lsp.buf.format()
-    require("conform").format()
-  end,
-})
+-- Note: Format on save is handled by conform.nvim via its format_on_save option
+-- If you need manual formatting for specific filetypes, uncomment below:
+-- autocmd("BufWritePre", {
+--   pattern = "*.lua",
+--   callback = function()
+--     require("conform").format()
+--   end,
+-- })
 
 -- Enable snacks.words for relevant buffers
 -- autocmd("BufEnter", {
@@ -15,7 +15,7 @@ autocmd("BufWritePre", {
 --     local buftype = vim.bo.buftype
 --     local filetype = vim.bo.filetype
 --     local bufname = vim.api.nvim_buf_get_name(0)
-    
+
 --     -- List of buffer types and filetypes to exclude
 --     local excluded_buftypes = {
 --       "nofile",    -- scratch buffers
@@ -23,26 +23,26 @@ autocmd("BufWritePre", {
 --       "terminal",  -- terminal buffers
 --       "quickfix",  -- quickfix buffers
 --     }
-    
+
 --     local excluded_filetypes = {
 --       "help",      -- help buffers
 --       "qf",        -- quickfix
 --       "dashboard", -- dashboard
 --     }
-    
+
 --     -- List of buffer names to exclude
 --     local excluded_bufnames = {
 --       "snacks_dashboard",
 --       "NvimTree",
 --       "Trouble",
 --     }
-    
+
 --     -- Check if current buffer should be excluded
---     local should_exclude = 
+--     local should_exclude =
 --       vim.tbl_contains(excluded_buftypes, buftype) or
 --       vim.tbl_contains(excluded_filetypes, filetype) or
 --       vim.tbl_contains(excluded_bufnames, vim.fn.fnamemodify(bufname, ":t"))
-    
+
 --     if not should_exclude then
 --       require("snacks").words.enable()
 --     end
@@ -55,7 +55,7 @@ autocmd("BufWritePre", {
 --     local buftype = vim.bo.buftype
 --     local filetype = vim.bo.filetype
 --     local bufname = vim.api.nvim_buf_get_name(0)
-    
+
 --     -- List of buffer types and filetypes to exclude
 --     local excluded_buftypes = {
 --       "nofile",    -- scratch buffers
@@ -64,14 +64,14 @@ autocmd("BufWritePre", {
 --       "quickfix",  -- quickfix buffers
 --       "nofile",    -- scratch buffers
 --     }
-    
+
 --     local excluded_filetypes = {
 --       "help",      -- help buffers
 --       "qf",        -- quickfix
 --       "dashboard", -- dashboard
 --       "man",       -- man pages
 --     }
-    
+
 --     -- List of buffer names to exclude
 --     local excluded_bufnames = {
 --       "snacks_dashboard",
@@ -80,13 +80,13 @@ autocmd("BufWritePre", {
 --       "TelescopePrompt",
 --       "DressingInput",
 --     }
-    
+
 --     -- Check if current buffer should be excluded
---     local should_exclude = 
+--     local should_exclude =
 --       vim.tbl_contains(excluded_buftypes, buftype) or
 --       vim.tbl_contains(excluded_filetypes, filetype) or
 --       vim.tbl_contains(excluded_bufnames, vim.fn.fnamemodify(bufname, ":t"))
-    
+
 --     if not should_exclude then
 --       require("snacks").scroll.enable()
 --     end
